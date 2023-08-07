25ea84dd8d16e30e2f98dfbc0eec5f10c53c913f
Vadim Burtelov <v@burtelov.ru>
2023-08-07 23:54:27 +0300

    Init Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem, PrimeNGConfig} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'angular-spa';
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  constructor(private primengConfig: PrimeNGConfig) {}
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.items = [
+      {
+        label: 'Создать',
+        icon: 'pi pi-fw pi-file'
+      },
+      {
+        label: 'Редактировать',
+        icon: 'pi pi-fw pi-pencil',
+      },
+      {
+        label: 'Удалить',
+        icon: 'pi pi-fw pi-user',
+      }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+// Import PrimeNG modules
+
+import {AppComponent} from "./app.component";
+import {MenubarModule} from "primeng/menubar";
+import {ButtonModule} from "primeng/button";
+import {CardModule} from "primeng/card";
+import {ImageModule} from "primeng/image";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    RouterModule.forRoot([{path: '', component: AppComponent}]),
+    ButtonModule,
+    CardModule,
+    ImageModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule { }
+
+export class AppModule {
+}
